353e8f8651359522cc7a390dea03b682fd9bfc70
wenjing.liu <liuwenjing.12@163.com>
2021-12-16 11:02:36 +0800

    delete invail file

--- seniorapplicationplugin/src/main/kotlin/com/wj/gradle/seniorapplication/tasks/transform/LabelClassVisitor.kt
+++ seniorapplicationplugin/src/main/kotlin/com/wj/gradle/seniorapplication/tasks/transform/LabelClassVisitor.kt
@@ -78,27 +78,5 @@ open class LabelClassVisitor(private val visitor: ClassVisitor) :
         methodVisitor.visitInsn(Opcodes.RETURN)
         methodVisitor.visitEnd()
     }
-
-//    override fun visitMethod(
-//        access: Int,
-//        name: String?,
-//        descriptor: String?,
-//        signature: String?,
-//        exceptions: Array<out String>?
-//    ): MethodVisitor {
-//        SystemPrint.outPrintln(TAG, "name = " + name)
-//
-//        if (FOR_LABEL != name) {
-//            return super.visitMethod(access, name, descriptor, signature, exceptions)
-//        }
-//        return LabelAdviceAdapter(
-//            api,
-//            super.visitMethod(access, name, descriptor, signature, exceptions),
-//            access,
-//            name,
-//            descriptor
-//        )
-//
-//    }
-
+    
 }
