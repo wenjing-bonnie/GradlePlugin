c3138c0a738f951ec611a386991219536f8e198f
wenjing.liu <liuwenjing.12@163.com>
2022-01-29 23:29:13 +0800

    get apk ok

--- pluginCode/wjapkprotect/src/main/kotlin/com/wj/gradle/apkprotect/tasks/unzip/UnzipApkIncrementalTask.kt
+++ pluginCode/wjapkprotect/src/main/kotlin/com/wj/gradle/apkprotect/tasks/unzip/UnzipApkIncrementalTask.kt
@@ -3,15 +3,14 @@ package com.wj.gradle.apkprotect.tasks.unzip
 import com.android.build.gradle.internal.tasks.NewIncrementalTask
 import com.wj.gradle.apkprotect.extensions.ApkProtectExtension
 import com.wj.gradle.base.utils.SystemPrint
-import org.gradle.api.file.Directory
 import org.gradle.api.file.DirectoryProperty
-import org.gradle.api.file.RegularFileProperty
-import org.gradle.api.tasks.InputFile
+import org.gradle.api.tasks.InputDirectory
 import org.gradle.api.tasks.OutputDirectory
 import org.gradle.work.Incremental
 import org.gradle.work.InputChanges
 import java.io.File
 import java.lang.IllegalArgumentException
+import java.util.regex.Pattern
 
 /**
  * 增量编译,可直接创建并行Task
@@ -29,20 +28,28 @@ abstract class UnzipApkIncrementalTask : NewIncrementalTask() {
         const val TAG: String = "UnzipApkIncrementalTask"
     }
 
-    //必须在实例化该Task通过set进行赋值
+    //必须在实例化该Task通过set进行赋值,否则会抛出异常
     @get:Incremental
-    @get:InputFile
+    @get:InputDirectory
     abstract val lazyApkDirectory: DirectoryProperty
 
     @get:OutputDirectory
     abstract val unzipDirectory: DirectoryProperty
 
+    /**
+     * 存放所有的apk文件
+     */
+    private val allApks = mutableListOf<File>()
+
     override fun doTaskAction(inputChanges: InputChanges) {
         SystemPrint.outPrintln(TAG, "The unZip begin ...")
         val workqueue = workerExecutor.noIsolation()
-
+        allApks.clear()
         val apkDirectory = lazyApkDirectory.get().asFile
         getAllApks(apkDirectory)
+        for (file in allApks) {
+            SystemPrint.outPrintln(TAG, file.path)
+        }
         // if (!apkDirectory.exists()) {
         // lazyApkFile.set(File())
         //  }
@@ -82,18 +89,23 @@ abstract class UnzipApkIncrementalTask : NewIncrementalTask() {
     /**
      * 获取[ //build/outputs/apk/huawei/debug]目录下所有变体下的apk
      */
-    private fun getAllApks(apkDirectory: File): List<File> {
-        val apkList = listOf<File>()
+    private fun getAllApks(apkDirectory: File) {
         //  variantName
-        SystemPrint.outPrintln(apkDirectory.path + " \n " + apkDirectory.exists() + "\n" + apkDirectory.isDirectory)
-        if (!apkDirectory.exists() || !apkDirectory.isDirectory) {
-            throw IllegalArgumentException("The apk directory is invalid !")
+        if (!apkDirectory.exists()) {
+            throw IllegalArgumentException("The apk directory is not exist !")
         }
         val files = apkDirectory.listFiles()
+        if (files == null || files.isEmpty()) {
+            return
+        }
         for (file in files) {
             SystemPrint.outPrintln(TAG, file.path)
+            if (file.isDirectory) {
+                getAllApks(file)
+            } else if (file.isFile && file.name.endsWith(".apk")) {
+                allApks.add(file)
+            }
         }
-        return apkList
     }
 
     /**
@@ -108,14 +120,61 @@ abstract class UnzipApkIncrementalTask : NewIncrementalTask() {
      * 默认的.apk存放的路径.
      */
     private fun getApkDefaultDirectory(): File {
-        val defaultPath =
-            "${project.projectDir.absolutePath}/build/outputs/apk/"
+        val defaultPath = getApkDefaultPath()
         val defaultDirectory = project.file(defaultPath)
-        //SystemPrint.outPrintln("default = " + defaultDirectory.path)
         if (!defaultDirectory.exists()) {
             defaultDirectory.mkdirs()
         }
         return defaultDirectory
+    }
+
+    /**
+     * 获取android studio默认的debug/release的apk存放的路径
+     */
+    private fun getApkDefaultPath(): String {
+        val rootPath = "${project.projectDir.absolutePath}/build/outputs/apk/"
+        return if (getApkPathByVariantName().isEmpty()) {
+            rootPath
+        } else if (isDebugBuildType()) {
+            "${rootPath}${getApkPathByVariantName()}/debug"
+        } else if (isReleaseBuildType()) {
+            "${rootPath}${getApkPathByVariantName()}/release"
+        } else {
+            rootPath
+        }
+    }
+
+    /**
+     * 从默认的debug/release的获取productFlavors
+     */
+    private fun getApkPathByVariantName(): String {
+        var productFlavors = ""
+        val regex = if (isDebugBuildType()) {
+            "(\\w+)Debug"
+        } else {
+            "(\\w+)Release"
+        }
+        val pattern = Pattern.compile(regex)
+        val matcher = pattern.matcher(variantName)
+        if (matcher.find()) {
+            //group(0)整个字符串;group(1)第一个括号内的内容;group(2)第二个括号内的内容
+            productFlavors = matcher.group(1)
+        }
+        return productFlavors
+    }
+
+    /**
+     * 是debug状态
+     */
+    private fun isDebugBuildType(): Boolean {
+        return variantName.endsWith("Debug")
+    }
+
+    /**
+     * 是release状态
+     */
+    private fun isReleaseBuildType(): Boolean {
+        return variantName.endsWith("Release")
     }
 
     /**
