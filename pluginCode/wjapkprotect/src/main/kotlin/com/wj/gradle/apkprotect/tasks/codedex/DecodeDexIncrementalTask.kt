8c8eb40aaf57bd73277b3afb620d2f135632b0bf
wenjing.liu <liuwenjing.12@163.com>
2022-02-04 10:40:23 +0800

    modify the gradle conifguration

--- pluginCode/wjapkprotect/src/main/kotlin/com/wj/gradle/apkprotect/tasks/codedex/DecodeDexIncrementalTask.kt
+++ pluginCode/wjapkprotect/src/main/kotlin/com/wj/gradle/apkprotect/tasks/codedex/DecodeDexIncrementalTask.kt
@@ -3,6 +3,7 @@ package com.wj.gradle.apkprotect.tasks.codedex
 import com.wj.gradle.apkprotect.tasks.codedex.parallel.DecodeDexAction
 import com.wj.gradle.apkprotect.tasks.codedex.parallel.EncodeDexWorkParameters
 import com.wj.gradle.base.utils.SystemPrint
+import groovy.lang.Closure
 import org.gradle.api.Action
 import org.gradle.api.Task
 import org.gradle.workers.WorkQueue
@@ -12,7 +13,7 @@ import java.io.File
  * 对apk里面解压之后的所有.dex进行解密
  */
 abstract class DecodeDexIncrementalTask : CodeDexTemplateIncrementalTask() {
-    
+
     companion object{
         val TAG: String
             get() = "DecodeDexIncrementalTask"
